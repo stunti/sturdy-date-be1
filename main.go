99cc767a29f3ab3ca6f5b5cd08baa1433bdd57b1
Olivier <olivier.bregeras@gmail.com>
2018-12-30 20:56:53 +0800

    Add basic go file

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/thepauleh/goserverless/serverless"
+)
+
+func main() {
+
+	// Create a new CloudFormation template
+    template := serverless.NewTemplate("myService")
+    
+    template.Service = "myService"
+
+    template.Provider = &serverless.Provider{
+        Name: "aws",
+        Runtime: "nodejs6.10",
+        MemorySize: 512,
+        Timeout: 10,
+        VersionFunctions: false
+    }
+
+	// An example function
+	template.Functions["users"] = &serverless.AWSServerlessFunction{
+		Handler: "service.o",
+        Name:   "${self:provider.stage}-users",
+        Description: "Description of what the lambda function does",
+        Runtime: "go1.x",
+        MemorySize: 128,
+        ReservedConcurrency: 5,
+        Timeout: 30,
+		Events: []serverless.Events{
+			serverless.HttpEvent{
+				Path: "users/create",
+				Method: "post",
+			},
+		},
+	}
+
+	y, err := template.YAML()
+	if err != nil {
+		fmt.Printf("Failed to generate YAML: %s\n", err)
+	} else {
+		fmt.Printf("%s\n", string(y))
+	}
+}
